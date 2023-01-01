d1eb9e8b14358490cfed4c05b8c38f99392f26e2
Ronak Mehta <rmehta2510@gmail.com>
2023-01-01 23:09:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 //app.use(express.json())
 //app.use(express.urlencoded({ extended: true }))

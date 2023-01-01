f54ae56eabc8617d95a8e78627bcbbfed3d91c08
Ronak Mehta <rmehta2510@gmail.com>
2023-01-01 22:49:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
 
-app.use(express.json())
+//app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // #############################################################################
@@ -26,12 +26,12 @@ app.post('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   //console.log("value = "+ JSON.parse(Object.keys(req.body)[0]).Text);
-  console.log("value = "+ JSON.stringify(req.body.Text));
+  console.log("value = "+ req.body.Text);
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
   
   //const item = await db.collection(col).set(key, JSON.parse(Object.keys(req.body)[0]).Text)
   
-  const item = await db.collection(col).set(key, JSON.stringify(req.body.Text))
+  const item = await db.collection(col).set(key, req.body.Text)
   
   //console.log(JSON.stringify(item, null, 2))
   console.log(item)

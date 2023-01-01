1907706c0c65ee96130ddf793db49d05bf4d56bb
Ronak Mehta <rmehta2510@gmail.com>
2023-01-01 21:26:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ app.post('/:col/:key', async (req, res) => {
 
   const col = req.params.col
   const key = req.params.key
-  console.log(req.body.Text);
+  console.log("value = "+ req.body[Object.keys(req.body)[0]]);
+  
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body.Text)
+  const item = await db.collection(col).set(key, req.body[Object.keys(req.body)[0]])
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })

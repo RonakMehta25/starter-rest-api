5fe8ed1ea3a018adf8af1ecc497370e18526464d
Ronak Mehta <rmehta2510@gmail.com>
2023-01-01 23:44:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,17 +29,38 @@ app.post('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   //console.log("value = "+ JSON.parse(Object.keys(req.body)[0]).Text);
-  console.log("req.body.Text = "+ req.body.Text);
-  console.log("JSON.stringify(req.body.Text) = "+ JSON.stringify(req.body.Text));
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+  //console.log("req.body.Text = "+ req.body.Text);
+  //console.log("JSON.stringify(req.body.Text) = "+ JSON.stringify(req.body.Text));
+  //console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
   
   //const item = await db.collection(col).set(key, JSON.parse(Object.keys(req.body)[0]).Text)
   
-  const item = await db.collection(col).set(key, JSON.stringify(req.body.Text))
+  //const item = await db.collection(col).set(key, JSON.stringify(req.body.Text))
   
   //console.log(JSON.stringify(item, null, 2))
-  console.log(item)
-  res.json(item).end()
+  //console.log(item)
+  //res.json(item).end()
+  
+  try {
+    // Extract the text from the request body
+    const text = req.body.Text;
+
+    // Set the params for the putItem operation
+    const params = {
+      TableName: 'brave-jade-henCyclicDB',
+      Item: {
+         'Text': {S: text},
+         'Value': {S: text}
+      }
+    };
+
+    // Save the item to the DynamoDB table
+    await db.putItem(params).promise();
+
+    res.status(200).send('Successfully saved item to DynamoDB table');
+  } catch (err) {
+    res.status(500).send(JSON.stringify(err));
+  }
 })
 
 // Delete an item
